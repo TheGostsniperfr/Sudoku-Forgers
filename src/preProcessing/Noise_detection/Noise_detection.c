1a24f51fc05b9e7b3b76f96274e27847e8658492
[Pseudo] <victor-gardere@orange.fr>
2023-11-03 14:33:02 +0100

    Update Noise_detection.c

--- src/preProcessing/Noise_detection/Noise_detection.c
+++ src/preProcessing/Noise_detection/Noise_detection.c
@@ -10,6 +10,6 @@ GridCell* noise_detetcion(GridCell* images)
 	GridCell* clean_img =  malloc(81 * sizeof(GridCell));
 	for (size_t i = 0; i < 81; i++)
 	{
-		clean_img[i]->image = Blob(images[i]->image, 1);
+		clean_img[i].image = Blob(images[i].image, 1);
 	}
 }
