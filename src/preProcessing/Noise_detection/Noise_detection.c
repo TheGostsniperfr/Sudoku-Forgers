7cfc6fe1e5951f260c6109482eea1c49ed356fc7
[Pseudo] <victor-gardere@orange.fr>
2023-11-03 13:25:49 +0100

    Update Noise_detection.c

--- src/preProcessing/Noise_detection/Noise_detection.c
+++ src/preProcessing/Noise_detection/Noise_detection.c
@@ -3,10 +3,19 @@
 #include "preProcessing/SDL_Function/sdlFunction.h"
 #include "preProcessing/BlobDetection/BlobDetection.h"
 
-void noise_detetcion(SDL_Surface** images)
+
+typedef struct GridCell {
+	(int, int) position;
+	SDL_Surface* image;
+	int size = 28;
+}GridCell;
+
+
+GridCell* noise_detetcion(SDL_Surface** images)
 {
+	GridCell* clean_img =  malloc(9 * sizeof(GridCell));
 	for (size_t i = 0; i < 9; i++)
 	{
-			Blob(images[i]);
+		Blob(images[i], 1);
 	}
 }
