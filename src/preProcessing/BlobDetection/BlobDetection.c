a3a739acdcd411169b7f75b65fe410dce94a5290
Gahyeji <ronan.argences@gmail.com>
2023-11-04 18:44:43 +0100

    Probleme matrice

--- src/preProcessing/BlobDetection/BlobDetection.c
+++ src/preProcessing/BlobDetection/BlobDetection.c
@@ -1,6 +1,6 @@
 #include <SDL2/SDL.h>
 #include "SDL2/SDL_image.h"
-#include "../../../include/preProcessing/SDL_Function/sdlFunction.h"
+#include "preProcessing/SDL_Function/sdlFunction.h"
 #include <err.h>
 
 typedef struct Point{
@@ -26,8 +26,8 @@ typedef struct Point{
  * 		- (int) : Size of the blob
 ******************************************************************************/
 
-int Fill(SDL_Surface *src, Uint32* pixels, int x, int y, Uint8 R, 
-Uint8 G, Uint8 B, char** blob) {
+int Fill(SDL_Surface *src, Uint32* pixels, int x, int y, int x2, int y2, Uint8 R, 
+Uint8 G, Uint8 B, char** blob, char Func) {
 	//Variables for color comparison and result
 	Uint8 r = 0;
 	Uint8 g = 0;
@@ -36,7 +36,10 @@ Uint8 G, Uint8 B, char** blob) {
 	Uint32 rgb = r + g + b;
 	int res = 1;
 
-	blob[x][y] = 1;
+	x2 = 1;
+	if(Func==1){
+		pixels[y* src->w +x] = SDL_MapRGB(src->format, 0, 0, 0);
+	}
 	//Iterates through the blob's neighbours to find neighbours and colors them
 	//in blue
 	if (((x>=0) && (x<src->w))&&((y>=0)&&(y<src->h)))
@@ -44,14 +47,15 @@ Uint8 G, Uint8 B, char** blob) {
 		SDL_GetRGB(pixels[y*src->w +  x], src->format, &r, &g, &b);
 		rgb = r + g + b;
 		if (rgb == rgb2){
-			res = res +Fill(src, pixels, x, y+1, R, G, B, blob);
-			res = res +Fill(src, pixels, x, y-1, R, G, B, blob);
-			res = res +Fill(src, pixels, x+1, y, R, G, B, blob);
-			res = res +Fill(src, pixels, x-1, y, R, G, B, blob);
-			res = res +Fill(src, pixels, x+1, y+1, R, G, B, blob);
-			res = res +Fill(src, pixels, x-1, y+1, R, G, B, blob);
-			res = res +Fill(src, pixels, x-1, y-1, R, G, B, blob);
-			res = res +Fill(src, pixels, x+1, y-1, R, G, B, blob);
+
+			res = res +Fill(src, pixels, x, y+1, x2, y2+1,R, G, B, blob, Func);
+			res = res +Fill(src, pixels, x, y-1, x2, y2-1,R, G, B, blob, Func);
+			res = res +Fill(src, pixels, x+1, y, x2+1, y2, R, G, B, blob, Func);
+			res = res +Fill(src, pixels, x-1, y, x2-1, y2, R, G, B, blob, Func);
+			res = res +Fill(src, pixels, x+1, y+1, x2+1, y2+1,R, G, B, blob, Func);
+			res = res +Fill(src, pixels, x-1, y+1, x2-1, y2+1, R, G, B, blob, Func);
+			res = res +Fill(src, pixels, x-1, y-1, x2-1, y2-1, R, G, B, blob, Func);
+			res = res +Fill(src, pixels, x+1, y-1, x2+1, y2-1, R, G, B, blob, Func);
 		}
 	}
 	return res;
@@ -71,14 +75,14 @@ SDL_Surface* Blob(SDL_Surface* src, int* size_max){
 	//creates pixel array from the image
 	Uint32* pixels = src->pixels;
 
-	char** blob;
-	int len = 0;
+	char** blob = malloc(sizeof(char*)*src->h);
 	Point max;
 
 
-	for(int i = 0; i<src->h; i++){
-		for(int j = 0;j<src->w;j++){
-			blob[j][i] = 0;
+	for(int i = blob; i<src->h; i++){
+		i = malloc(sizeof(char)*src->w);
+		for(int *j = i;j<src->w;j++){
+			*j = 0;
 		}
 	}
 
@@ -97,14 +101,28 @@ SDL_Surface* Blob(SDL_Surface* src, int* size_max){
 	Uint8 g = 0;
 	Uint8 b = 0;
 	Uint32 rgb = 0;
+	int y = 0;
+	int* x = 0;
 
 	//runs through the image to find the biggest blob
 	for(int i = 0; i<src->h;i++){
+		if(y ==0){
+			y = blob;
+		}
+		else{
+			y+=1;
+		}
 		for(int j =0;j<src->w;j++){
+			if(x==0){
+				x = y;
+			}
+			else{
+				x++;
+			}
 			SDL_GetRGB(pixels[i*src->w +  j], src->format, &r, &g, &b);
 			rgb = (r+g+b)/3;
 			if (rgb==0){
-				size = Fill(src, pixels, j, i, r, g, b, blob);
+				size = Fill(src, pixels, j, i, x, y, r, g, b, blob, 0);
 				Point temp = {j, i, size};
 				if (size>area_max){
 					area_max = size;
@@ -118,15 +136,19 @@ SDL_Surface* Blob(SDL_Surface* src, int* size_max){
 	src = SDL_CreateRGBSurface(0, src->w, src->h, src->format->BitsPerPixel,
      src->format->Rmask, src->format->Gmask, src->format->Bmask, src->format->Amask);
 	pixels = src->pixels;
+	for (int i = 0; i<src->h;i++){
+		for(int j = 0; j<src->w; j++){
+			pixels[i* src->w +j] = SDL_MapRGB(src->format, 0, 0, 0);
+		}
+	}
 	//Cleans smallest blobs
 	for (int i = 0; i<src->h;i++){
 		for(int j = 0; j<src->w; j++){
-			if(blob[j][i]){
-				
+			if((j==max.x)&&(i==max.y)){
+				Fill(src, pixels, j, i, x, y, 255,  255, 255, blob, 1);
 			}
 		}
 	}
-	printf("ok pour small blob\n");
 	src = SDL_CreateRGBSurfaceFrom(pixels,
 	src->w,
 	src->h,
@@ -141,18 +163,4 @@ SDL_Surface* Blob(SDL_Surface* src, int* size_max){
 	//modifies pointer size and returns the surface
 	*size_max = area_max;
 	return src;
-}
-
-int main (int argc, char* argv[]){
-	if (argc!= 2){
-		return 1;
-	}
-	SDL_Surface* src = IMG_Load(argv[1]);
-	if (src == NULL){
-		return 1;
-	}
-	src = SDL_ConvertSurfaceFormat(src, SDL_PIXELFORMAT_ABGR8888, 0);
-	int size = 0;
-	Blob(src, &size);
-	printf("%d\n", size);
 }
