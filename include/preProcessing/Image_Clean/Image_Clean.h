dce37cfddb48b85a9b0d4c456fee4089cbfec57c
Papi Victor <victor-gardere@orange.fr>
2023-11-24 18:20:35 +0100

    Image_Clean (pitié ça marche)

--- include/preProcessing/Image_Clean/Image_Clean.h
+++ include/preProcessing/Image_Clean/Image_Clean.h
@@ -3,13 +3,17 @@
 
 #include <SDL2/SDL.h>
 #include "SDL2/SDL_image.h"
-#include "../../../include/preProcessing/SDL_Function/sdlFunction.h"
+#include "preProcessing/SDL_Function/sdlFunction.h"
 
 #include "preProcessing/BlobDetection/BlobDetection.h"
 #include "preProcessing/CaseDetection/case_detection.h"
 
 void reversedBW(SDL_Surface* image);
 
-GridCell* NoiseDetection(GridCell* images);
+void clean_edge(SDL_Surface* image);
+
+void clean_all(SDL_Surface* image);
+
+void Image_Clean(GridCell* images);
 
 #endif
---
+++ src/preProcessing/Image_Clean/Image_Clean.c
@@ -0,0 +1,113 @@
+#include <SDL2/SDL.h>
+#include "SDL2/SDL_image.h"
+#include "preProcessing/SDL_Function/sdlFunction.h"
+#include "preProcessing/BlobDetection/BlobDetection.h"
+#include "preProcessing/CaseDetection/case_detection.h"
+
+void reversedBW(SDL_Surface* image)
+{
+	SDL_LockSurface(image);
+
+	Uint32* pixels = (Uint32*)image->pixels;
+
+	for (int y = 0; y < image->h; y++)
+        {
+        for (int x = 0; x < image->w; x++)
+        {
+            Uint32 pixel = pixels[y * image->w + x];
+            Uint8 r, g, b, a;
+            SDL_GetRGBA(pixel, image->format, &r, &g, &b, &a);
+
+            r = 255 - r;
+            g = 255 - g;
+            b = 255 - b;
+
+            pixels[y * image->w + x] = SDL_MapRGBA(image->format, r, g, b, a);
+        }
+    }
+
+    SDL_UnlockSurface(image);
+}
+
+void clean_edge(SDL_Surface* image)
+{   
+    SDL_LockSurface(image);
+
+    for (int y = 0; y < 3; ++y)
+    {
+        for (int x = 0; x < image->w; ++x)
+        {       
+            Uint32* pixel = (Uint32*)((Uint8*)image->pixels + y * image->pitch + x * sizeof(Uint32));
+            *pixel = SDL_MapRGB(image->format, 255, 255, 255);
+        }
+    }
+
+    for (int y = image->h - 3; y < image->h; ++y)
+    {
+        for (int x = 0; x < image->w; ++x)
+        {
+            Uint32* pixel = (Uint32*)((Uint8*)image->pixels + y * image->pitch + x * sizeof(Uint32));
+            *pixel = SDL_MapRGB(image->format, 255, 255, 255);
+        }
+    }
+
+    for (int y = 0; y < image->h; ++y)
+    {
+        for (int x = 0; x < 3; ++x)
+        {
+            Uint32* pixel = (Uint32*)((Uint8*)image->pixels + y * image->pitch + x * sizeof(Uint32));
+            *pixel = SDL_MapRGB(image->format, 255, 255, 255);
+        }
+    }
+
+    for (int y = 0; y < image->h; ++y) 
+    {
+        for (int x = image->w - 3; x < image->w; ++x) 
+        {
+            Uint32* pixel = (Uint32*)((Uint8*)image->pixels + y * image->pitch + x * sizeof(Uint32));
+            *pixel = SDL_MapRGB(image->format, 255, 255, 255);
+        }
+    }
+
+    SDL_UnlockSurface(image);
+}
+
+void clean_all(SDL_Surface* image) 
+{
+    SDL_LockSurface(image);
+
+    int width = image->w;
+    int height = image->h;
+
+    for (int y = 0; y < height; ++y) 
+    {
+        for (int x = 0; x < width; ++x) 
+        {
+            Uint32* pixel = (Uint32*)((Uint8*)image->pixels + y * image->pitch + x * sizeof(Uint32));
+            *pixel = SDL_MapRGB(image->format, 0, 0, 0);
+        }
+    }
+
+    SDL_UnlockSurface(image);
+}
+
+void Image_Clean(GridCell* images)
+{
+	int size = 0;
+    for (size_t i = 0; i < 2; i++)
+    {
+        clean_edge(images[i].image);
+        images[i].image = Blob(images[i].image, &size);
+        images[i].label = size;
+        if(size <= 40)
+        {
+            clean_all(images[i].image);
+            images[i].isDigit = 0;
+        }
+        else
+        {
+	        reversedBW(images[i].image);
+            images[i].isDigit = 1;
+        }
+    }
+}
