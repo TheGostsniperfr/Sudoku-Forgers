f5d00c61b3d7f2453a32a9e3d2b157217cc45f8c
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2023-11-25 20:04:24 +0100

    sync app

--- src/GUI/app.c
+++ src/GUI/app.c
@@ -1,7 +1,6 @@
 #include <gtk/gtk.h>
 #include <stdlib.h>
 
-
 void on_scale_value_changed(GtkRange *range , gpointer user_data) {
     GtkStack *pageContainer = GTK_STACK(user_data);
     gdouble value = gtk_range_get_value(range);
@@ -11,6 +10,20 @@ void on_scale_value_changed(GtkRange *range , gpointer user_data) {
         pageContainer,
         g_strdup_printf("page%.0f", value)
     );
+}
+
+void load_and_resize_image(const char *filename, int sizeX, int sizeY, GtkImage *img) {
+    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(filename, NULL);
+
+    if (pixbuf == NULL) {
+        g_printerr("Error to load image: %s\n", filename);
+        return;
+    }
+
+    GdkPixbuf *resized_pixbuf = gdk_pixbuf_scale_simple(pixbuf, sizeX, sizeY, GDK_INTERP_BILINEAR);
+    g_object_unref(pixbuf);
+    gtk_image_set_from_pixbuf(img, resized_pixbuf);
+    g_object_unref(resized_pixbuf);
 }
 
 void on_back_btn_pressed(GtkButton *button __attribute__((unused)),
@@ -28,6 +41,7 @@ void on_forward_btn_pressed(GtkButton *button __attribute__((unused)),
     gdouble value = gtk_range_get_value(GTK_RANGE(pageSlider));
     gtk_range_set_value(GTK_RANGE(pageSlider), CLAMP(value+1, 1, 6));
 }
+
 
 int main() {
 
@@ -56,6 +70,14 @@ int main() {
     GtkButton* forwardBtn =
         GTK_BUTTON(gtk_builder_get_object(builder, "forwardBtn"));
 
+    GtkImage* logoImg = GTK_IMAGE(gtk_builder_get_object(builder, "logo"));
+
+    GtkImage* inputImageResult = GTK_IMAGE(gtk_builder_get_object(builder, "inputImageResult"));
+    GtkImage* outputImageResult = GTK_IMAGE(gtk_builder_get_object(builder, "outputImageResult"));
+
+    load_and_resize_image("ressources/logo.png", 300, 300, logoImg);
+    load_and_resize_image("ressources/emptyGrid.png", 300, 300, inputImageResult);
+    load_and_resize_image("ressources/emptyGrid.png", 300, 300, outputImageResult);
 
     //Page Slider
     gtk_range_set_range(GTK_RANGE(pageSlider), 1, 6);
@@ -68,7 +90,6 @@ int main() {
         G_CALLBACK(on_back_btn_pressed), pageSlider);
     g_signal_connect(forwardBtn, "clicked",
         G_CALLBACK(on_forward_btn_pressed), pageSlider);
-
 
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
