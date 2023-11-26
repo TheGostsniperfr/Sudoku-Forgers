e0baf5f49c5a4ac46f08ba897c9bd1c6e53e47df
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2023-11-26 10:50:15 +0100

    [GUI] add edit grid + load img

--- src/GUI/app.c
+++ src/GUI/app.c
@@ -1,5 +1,38 @@
 #include <gtk/gtk.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+#define EDIT_GRID_SIZE 9
+GdkPixbuf *originalImg = NULL;
+
+GtkEntry *editGridMat[EDIT_GRID_SIZE][EDIT_GRID_SIZE];
+typedef struct {
+    int row;
+    int col;
+} EntryCoordinates;
+
+gboolean is_digit(const gchar *text) {
+    return (text != NULL && *text != '\0' && isdigit(*text));
+}
+
+void on_file_selected(GtkFileChooserButton *filechooserbutton,
+    gpointer user_data)
+{
+    const gchar *filename =
+        gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(filechooserbutton));
+
+    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(filename, NULL);
+
+    if (originalImg != NULL) {
+        g_object_unref(originalImg);
+    }
+    originalImg = pixbuf;
+
+    GtkWidget *image = GTK_WIDGET(user_data);
+    gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf);
+
+    g_free((gpointer)filename);
+}
 
 void on_scale_value_changed(GtkRange *range , gpointer user_data) {
     GtkStack *pageContainer = GTK_STACK(user_data);
@@ -12,7 +45,26 @@ void on_scale_value_changed(GtkRange *range , gpointer user_data) {
     );
 }
 
-void load_and_resize_image(const char *filename, int sizeX, int sizeY, GtkImage *img) {
+void on_insert_text(GtkEditable *editable, const gchar *text,
+    gint length __attribute__((unused)),
+    gint *position __attribute__((unused)), gpointer user_data)
+{
+    GtkEntry *entry = GTK_ENTRY(editable);
+    EntryCoordinates *coords = (EntryCoordinates *)user_data;
+    if (!is_digit(text)) {
+        g_signal_stop_emission_by_name(G_OBJECT(editable), "insert-text");
+        return;
+    }
+
+    g_print("Input edit grid [%d][%d] : %s\n",
+        coords->row, coords->col, text);
+
+    (void)entry;
+}
+
+void load_and_resize_image(const char *filename, int sizeX,
+    int sizeY, GtkImage *img)
+{
     GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(filename, NULL);
 
     if (pixbuf == NULL) {
@@ -20,7 +72,9 @@ void load_and_resize_image(const char *filename, int sizeX, int sizeY, GtkImage 
         return;
     }
 
-    GdkPixbuf *resized_pixbuf = gdk_pixbuf_scale_simple(pixbuf, sizeX, sizeY, GDK_INTERP_BILINEAR);
+    GdkPixbuf *resized_pixbuf = gdk_pixbuf_scale_simple(pixbuf, sizeX,
+        sizeY, GDK_INTERP_BILINEAR);
+
     g_object_unref(pixbuf);
     gtk_image_set_from_pixbuf(img, resized_pixbuf);
     g_object_unref(resized_pixbuf);
@@ -70,14 +124,20 @@ int main() {
     GtkButton* forwardBtn =
         GTK_BUTTON(gtk_builder_get_object(builder, "forwardBtn"));
 
-    GtkImage* logoImg = GTK_IMAGE(gtk_builder_get_object(builder, "logo"));
+    GtkFileChooserButton* fileChooseBtn =
+        GTK_FILE_CHOOSER_BUTTON(
+            gtk_builder_get_object(builder, "chooseFileBtn"));
 
-    GtkImage* inputImageResult = GTK_IMAGE(gtk_builder_get_object(builder, "inputImageResult"));
-    GtkImage* outputImageResult = GTK_IMAGE(gtk_builder_get_object(builder, "outputImageResult"));
+    GtkImage* logoImg =
+        GTK_IMAGE(gtk_builder_get_object(builder, "logo"));
+    GtkImage* inputImageResult =
+        GTK_IMAGE(gtk_builder_get_object(builder, "inputImageResult"));
+    GtkGrid* matrixGrid =
+        GTK_GRID(gtk_builder_get_object(builder, "editGrid"));
 
     load_and_resize_image("ressources/logo.png", 300, 300, logoImg);
-    load_and_resize_image("ressources/emptyGrid.png", 300, 300, inputImageResult);
-    load_and_resize_image("ressources/emptyGrid.png", 300, 300, outputImageResult);
+    load_and_resize_image
+        ("ressources/emptyGrid.png", 300, 300, inputImageResult);
 
     //Page Slider
     gtk_range_set_range(GTK_RANGE(pageSlider), 1, 6);
@@ -91,9 +151,44 @@ int main() {
     g_signal_connect(forwardBtn, "clicked",
         G_CALLBACK(on_forward_btn_pressed), pageSlider);
 
+    //File chooser btn
+    g_signal_connect(fileChooseBtn, "selection-changed",
+        G_CALLBACK(on_file_selected), inputImageResult);
+
+    //grid manager
+    for (int i = 0; i < EDIT_GRID_SIZE; i++) {
+        for (int j = 0; j < EDIT_GRID_SIZE; j++) {
+            editGridMat[i][j] = GTK_ENTRY(gtk_entry_new());
+            gtk_grid_attach(matrixGrid,
+                GTK_WIDGET(editGridMat[i][j]), j, i, 1, 1);
+
+            gtk_entry_set_input_purpose
+                (editGridMat[i][j], GTK_INPUT_PURPOSE_DIGITS);
+            gtk_entry_set_max_length(editGridMat[i][j], 1);
+            gtk_entry_set_width_chars(editGridMat[i][j], 1);
+            gtk_entry_set_max_width_chars(editGridMat[i][j], 2);
+
+            EntryCoordinates *coords = g_new(EntryCoordinates, 1);
+            coords->row = i;
+            coords->col = j;
+
+            g_signal_connect(G_OBJECT(editGridMat[i][j]),
+                "insert-text", G_CALLBACK(on_insert_text), coords);
+
+        }
+    }
+
+
+
+    gtk_widget_show_all(GTK_WIDGET(matrixGrid));
+
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     gtk_main();
+
+    if (originalImg != NULL) {
+        g_object_unref(originalImg);
+    }
 
     return EXIT_SUCCESS;
 }
