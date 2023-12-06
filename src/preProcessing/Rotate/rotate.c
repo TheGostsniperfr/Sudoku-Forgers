38e1b210251b32b249e42679085abf097f3746ca
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2023-12-06 23:28:00 +0100

    [GUI] fix manual rotation
    Can now be used at an extreme angle

--- src/preProcessing/Rotate/rotate.c
+++ src/preProcessing/Rotate/rotate.c
@@ -8,80 +8,70 @@
 
 SDL_Surface* Rotated_image(SDL_Surface* image, double angle)
 {
-	//Get the dimension of the image
-	int w = image->w;
-	int h = image->h;
+    // Get the dimension of the image
+    int w = image->w;
+    int h = image->h;
 
-	//Get the center of the image
-	int center_x = w/2;
-	int center_y = h/2;
+    // Get the center of the image
+    int center_x = w / 2;
+    int center_y = h / 2;
 
-	//Create a new image for the rotated image
-	SDL_Surface* rotated_image = SDL_CreateRGBSurfaceWithFormat
-                        (
-                            0,
-                            w,
-                            h,
-                            32,
-                            SDL_PIXELFORMAT_ABGR8888
-                        );
+    // Create a new image for the rotated image
+    SDL_Surface* rotated_image = SDL_CreateRGBSurfaceWithFormat(
+        0,
+        w,
+        h,
+        32,
+        SDL_PIXELFORMAT_ABGR8888);
 
-	Uint32* rotated_pixels = (Uint32*) rotated_image->pixels;
-	//Basic colors
-	Uint32 whitePx = SDL_MapRGB(image->format, 255, 255, 255);
-	
-	for(int i = 0; i < rotated_image->h; i++)
-	{
-		for(int j = 0; j < rotated_image->w; j++)
-		{
-			rotated_pixels[i*rotated_image->w+j] = whitePx;
-		}
-	}
+    Uint32* rotated_pixels = (Uint32*)rotated_image->pixels;
 
+    // Basic colors
+    Uint32 whitePx = SDL_MapRGB(image->format, 255, 255, 255);
+
+    for (int i = 0; i < rotated_image->h; i++)
+    {
+        for (int j = 0; j < rotated_image->w; j++)
+        {
+            rotated_pixels[i * rotated_image->w + j] = whitePx;
+        }
+    }
 	//Get angle from degree to radian
-	double rad_angle = angle * (M_PI / 180);
+    double rad_angle = angle * (M_PI / 180);
 
-	//Get the angles for sin and tan
-	double angle_tan = tan(rad_angle/2);
+	//Get the angles for sin and cose
 	double angle_sin = sin(rad_angle);
+    double angle_cos = cos(rad_angle);
 
+    for (int i = 0; i < w; i++)
+    {
+        for (int j = 0; j < h; j++)
+        {
+            int new_x =
+				(i - center_x)
+				* angle_cos
+				- (j - center_y)
+				* angle_sin
+				+ center_x;
 
-	//Travel into the image to rotate it
-	for(int i = 0; i < w; i++)
-	{
-		for(int j = 0; j < h; j++)
-		{
-			int new_x;
-			int new_y;
+            int new_y =
+				(i - center_x)
+				* angle_sin
+				+ (j - center_y)
+				* angle_cos
+				+ center_y;
 
-			int x = i - center_x;
-			int y = j - center_y;
+            Uint32* pixels = (Uint32*)image->pixels;
 
-			//step 1
-			new_x = round(x - y*angle_tan);
-			new_y = y;
+            if (new_x >= 0 && new_x < w && new_y >= 0 && new_y < h)
+            {
+                Uint32 pixel = pixels[j * image->w + i];
+                rotated_pixels[new_y * rotated_image->w + new_x] = pixel;
+            }
+        }
+    }
 
-			//step 2
-			new_y = round(new_x *angle_sin+new_y);
-
-			//step 3
-			new_x = round(new_x-new_y*angle_tan);
-
-			new_y += center_y;
-			new_x += center_x;
-
-			Uint32* pixels = (Uint32*) image->pixels;
-
-			if(new_x >= 0 && new_x < w && new_y >= 0 && new_y < h)
-			{
-				Uint32 pixel = pixels[j * image->w + i];
-				rotated_pixels[new_y * rotated_image->w + new_x] = pixel;
-			}
-		}
-	}
-
-	//SDL_FreeSurface(image);
-	return rotated_image;
+    return rotated_image;
 }
 
 void Save_RotatedImg(SDL_Surface* image, double angle)
